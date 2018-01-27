7f41d2be760118b8a7ad5da36e33908538748472
Mihail Shumilov <mschumilow@gmail.com>
2018-01-27 22:36:54 +0200

    add settings

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "bitcoincarboncopy")
+set(CN_CURRENCY_DISPLAY_NAME "Bitcoin Carbon Copy")
+set(CN_CURRENCY_TICKER "BTCC")
